045d2be3334a8ca432909c9cf7688a20b65e785c
AlfredoGJ <josalfk@hotmail.com>
2021-04-15 00:09:50 -0500

    fixed theme on playvideo

--- src/components/pages/PlayVideo/PlayVideo.jsx
+++ src/components/pages/PlayVideo/PlayVideo.jsx
@@ -4,43 +4,46 @@ import DetailedVideoCard from '../../organisms/DetailedVideoCard/DetailedVideoCa
 import YoutubeEmbed from '../../atoms/YouTubeEmbed/YouTubeEmbed';
 import Text from '../../atoms/Text/Text';
 import Button from '../../atoms/Button/Button';
+import StyledPlayVideo from './PlayVideo.styled';
 
 const PlayVideo = ({ video, relatedVideos }) => {
   return (
-    <Grid padding={{ left: 3, right: 3, top: 6, bottom: 2 }} lg={12}>
-      <Grid lg={7} direction="column">
-        <Grid lg={12}>
-          <YoutubeEmbed embedId={video.id} />
-        </Grid>
-        <Grid padding={{ top: 1, bottom: 1 }}>
-          <Grid xl={10}>
-            <Text variant="h2">{video.title}</Text>
+    <StyledPlayVideo>
+      <Grid padding={{ left: 3, right: 3, top: 6, bottom: 2 }} lg={12}>
+        <Grid lg={7} direction="column">
+          <Grid lg={12}>
+            <YoutubeEmbed embedId={video.id} />
           </Grid>
+          <Grid padding={{ top: 1, bottom: 1 }}>
+            <Grid xl={10}>
+              <Text variant="h2">{video.title}</Text>
+            </Grid>
 
-          <Grid xl={2}>
-            <Button variant="primary">add to favorites</Button>
+            <Grid xl={2}>
+              <Button variant="primary">add to favorites</Button>
+            </Grid>
+          </Grid>
+          <Grid>
+            <Text variant="body2">{video.description}</Text>
           </Grid>
         </Grid>
-        <Grid>
-          <Text variant="body2">{video.description}</Text>
+        <Grid lg={5} xl={5} direction="column" padding={{ left: 2 }}>
+          {relatedVideos.map((relatedVideo) => {
+            return (
+              <DetailedVideoCard
+                data-testid="related-video"
+                id={relatedVideo.id}
+                videoId={relatedVideo.id}
+                key={relatedVideo.id}
+                videoTitle={relatedVideo.title}
+                thumbnail={relatedVideo.thumbnailMedium}
+                description={relatedVideo.description}
+              />
+            );
+          })}
         </Grid>
       </Grid>
-      <Grid lg={5} xl={5} direction="column" padding={{ left: 2 }}>
-        {relatedVideos.map((relatedVideo) => {
-          return (
-            <DetailedVideoCard
-              data-testid="related-video"
-              id={relatedVideo.id}
-              videoId={relatedVideo.id}
-              key={relatedVideo.id}
-              videoTitle={relatedVideo.title}
-              thumbnail={relatedVideo.thumbnailMedium}
-              description={relatedVideo.description}
-            />
-          );
-        })}
-      </Grid>
-    </Grid>
+    </StyledPlayVideo>
   );
 };
 
---
+++ src/components/pages/PlayVideo/PlayVideo.styled.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+const StyledPlayVideo = styled.div`
+  background-color: ${(props) => props.theme.colors.background};
+  min-height: 100vh;
+`;
+
+export default StyledPlayVideo;
